cb4bab4ecc6cb4ecd20a766dface1b9ddfb4b9de
Mark The Magnificent <contact@mnegretev.info>
2020-09-24 16:29:47 -0500

    Add script for ros noetic

---
+++ SetupNoetic.sh
@@ -0,0 +1,22 @@
+sudo apt-get install ros-noetic-tf2-bullet -y
+sudo apt-get install ros-noetic-map-server -y
+sudo apt-get install ros-noetic-amcl -y
+sudo apt-get install ros-noetic-move-base -y
+sudo apt-get install ros-noetic-random-numbers -y
+sudo apt-get install ros-noetic-moveit-kinematics -y
+sudo apt-get install ros-noetic-sound-play -y
+sudo apt-get install ros-noetic-dwa-local-planner
+sudo apt-get install ros-noetic-gazebo-dev 
+sudo apt-get install ros-noetic-gazebo-msgs 
+sudo apt-get install ros-noetic-gazebo-plugins
+sudo apt-get install ros-noetic-gazebo-ros
+sudo apt-get install ros-noetic-gazebo-ros-control
+sudo apt-get install ros-noetic-gazebo-ros-pkgs
+sudo apt-get install swig
+sudo apt-get install python-pip
+sudo apt-get install portaudio19-dev
+sudo apt-get install kolourpaint4
+sudo pip install pyaudio
+sudo pip install pocketsphinx
+sudo pip install opencv-python==3.4.2.16
+sudo pip install opencv-contrib-python==3.4.2.16
