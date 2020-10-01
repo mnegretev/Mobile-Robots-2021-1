446b0610066465b7a730df783d86b061bca9c99e
David Trejo <luisdavidtorrestrejo@gmail>
2020-10-01 17:18:14 -0500

    Prueba Nombre

--- catkin_ws/src/students/scripts/practice00.py
+++ catkin_ws/src/students/scripts/practice00.py
@@ -13,7 +13,7 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "Torres Trejo Luis"
+NAME = "Torres Trejo Luis David"
 
 def callback_laser_scan(msg):
     #
