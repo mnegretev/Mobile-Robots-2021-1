b2c31efc9cde7c6dd7b1147057299677907a2d72
Mark The Magnificent <contact@mnegretev.info>
2020-10-01 16:31:17 -0500

    Segunda modificaion de prueba

--- catkin_ws/src/students/scripts/practice00.py
+++ catkin_ws/src/students/scripts/practice00.py
@@ -13,7 +13,7 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "NEGRETE_VILLANUEVA"
+NAME = "NEGRETE_VILLANUEVA MARCO ANTONIO"
 
 def callback_laser_scan(msg):
     #
