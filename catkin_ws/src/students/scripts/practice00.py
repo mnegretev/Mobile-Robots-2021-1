b09621772183c98c5a3484c2a3bd4ab8b05bc61e
JoseOlivas <chocheman7@hotmail.com>
2020-10-07 19:16:08 -0500

    Práctica 00 terminada

--- catkin_ws/src/students/scripts/practice00.py
+++ catkin_ws/src/students/scripts/practice00.py
@@ -20,6 +20,18 @@ def callback_laser_scan(msg):
     # TODO:
     # Do something to detect if there is an obstacle in front of the robot.
     #
+    #print("Received laser scan with" + str(len(msg.ranges)))
+    #print("Angle min: " + str(msg.angle_min))
+    #print("Angle increment: " + str(msg.angle_increment))
+    index = int((0 - msg.angle_min)/msg.angle_increment)
+    #print("Index for 0 rad: " + str(index))
+    #print("Distance for 0rad: " + str(msg.ranges[index]))
+    global obstacle_detected
+    obstacle_detected = msg.ranges[index] < 1.0
+    #if(msg.ranges[index] < 1.0):
+    #    print("Warning! Obstacle detected")
+    #else:
+    #    print("No risk of collision")
     return
 
 def main():
@@ -28,6 +40,9 @@ def main():
     rospy.Subscriber("/scan", LaserScan, callback_laser_scan)
     pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     loop = rospy.Rate(10)
+
+    global obstacle_detected
+    obstacle_detected = True
     
     while not rospy.is_shutdown():
         #
@@ -36,6 +51,12 @@ def main():
         # Move forward if there is no obstacle in front and stop otherwise.
         # Publish the message.
         #
+        cmd_vel = Twist()
+        if not obstacle_detected:
+            cmd_vel.linear.x = 0.5
+        else:
+            cmd_vel.linear.x = 0
+        pub_cmd_vel.publish(cmd_vel)
         loop.sleep()
 
 
