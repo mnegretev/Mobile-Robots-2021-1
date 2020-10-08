db29fecb1c424fc56441fd97ecfd84447a88b510
Abraham Rangel <asrn5200@gmail.com>
2020-10-08 13:40:11 -0500

    pratcica00_terminada

--- catkin_ws/src/students/scripts/practice00.py
+++ catkin_ws/src/students/scripts/practice00.py
@@ -20,6 +20,24 @@ def callback_laser_scan(msg):
     # TODO:
     # Do something to detect if there is an obstacle in front of the robot.
     #
+    '''
+    print('Recived laser scan with ' + str(len(msg.ranges)))
+    print('Angle min: ' + str(msg.angle_min))
+    print('Angle Increment: ' + str(msg.angle_increment))
+    index = int((0-msg.angle_min)/msg.angle_increment)
+    print('Index for 0 rad: ' + str(index))
+    print('Distance at 0 [rad] : ' + str(index))
+    print('Distance  at 0 [rad]: ' + str(msg.ranges[index]))
+    '''
+    index = int((0 - msg.angle_min)/msg.angle_increment)
+   
+    global obstacule_detected
+    obstacule_detected = msg.ranges[index] < 1.0
+
+    #if msg.ranges[index] < 1.0:
+    #    print('Warning obstacule detected')
+    #else:
+    #    print('No risk of collision')
     return
 
 def main():
@@ -29,13 +47,22 @@ def main():
     pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     loop = rospy.Rate(10)
     
+
+    global obstacule_detected
+    obstacule_detected = True
     while not rospy.is_shutdown():
         #
         # TODO:
         # Declare a Twist message and assign the appropiate speeds:
-        # Move forward if there is no obstacle in front and stop otherwise.
+        # Move forward if there is no obstacle in front and stop       otherwise.
         # Publish the message.
         #
+        cmd_vel = Twist()
+        if not obstacule_detected:
+           cmd_vel.linear.x = 0.5
+        else:
+           cmd_vel.linear.x = 0
+        pub_cmd_vel.publish(cmd_vel)
         loop.sleep()
 
 
