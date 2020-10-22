0b4926503d0f7bb2bc3975d8619fb6b97fe5ef3c
wexujk <carlospervas@gmail.com>
2020-10-22 00:53:04 -0500

    preba

--- catkin_ws/src/students/scripts/practice00.py
+++ catkin_ws/src/students/scripts/practice00.py
@@ -13,7 +13,7 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "NEGRETE_VILLANUEVA MARCO ANTONIO"
+NAME = "Perez Vasquez Carlos"
 
 def callback_laser_scan(msg):
     #
