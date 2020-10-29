708191be487bcf22a9dd41fc6e4a2252e9662337
Mariana Arguelles Macosay <marianita_maple@comunidad.unam.mx>
2020-10-29 02:30:23 -0600

    Practica 02 Completa

--- catkin_ws/src/students/scripts/practice02.py
+++ catkin_ws/src/students/scripts/practice02.py
@@ -18,7 +18,7 @@ from nav_msgs.msg import Path
 from nav_msgs.srv import *
 from collections import deque
 
-NAME = "ARGUELLES_MACOSAY"
+NAME = "ARGUELLES_MACOSAY" 
 
 def dijkstra(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
     #
