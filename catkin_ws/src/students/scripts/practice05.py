12c88e933f0ed62091bbc3ccac11586c72b458c5
Mariana Arguelles Macosay <marianita_maple@comunidad.unam.mx>
2020-11-26 00:18:46 -0600

    correccion de signo

--- catkin_ws/src/students/scripts/practice05.py
+++ catkin_ws/src/students/scripts/practice05.py
@@ -54,10 +54,10 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     error_a= math.sqrt( pow(dif_x , 2) + pow(dif_y , 2) )
 
     if (error_a > math.pi): 
-        error_a = -math.pi
+        error_a += -2*math.pi
         print("pi llego a ser mayor")
     if (error_a < -math.pi):
-        error_a = math.pi
+        error_a += +2*math.pi
         print("pi llego a ser menor")
     v = v_max * math.exp(-error_a * error_a/alpha)
     w = w_max * (2/(1 + math.exp(-error_a/beta)) - 1)
