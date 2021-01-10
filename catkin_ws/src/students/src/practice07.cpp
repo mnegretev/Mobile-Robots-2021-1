602b04309ecce2662cb06418b1b22a674e4756fa
Mariana Arguelles Macosay <marianita_maple@comunidad.unam.mx>
2021-01-10 13:05:05 -0600

    Pratica 07 Terminada

--- catkin_ws/src/students/src/practice07.cpp
+++ catkin_ws/src/students/src/practice07.cpp
@@ -17,7 +17,7 @@
 #include "geometry_msgs/Pose2D.h"
 #include "tf/transform_broadcaster.h"
 
-#define NOMBRE "ARGUELLES_MACOSAY"
+#define NOMBRE "ARGUELLES_MACOSAY" //Practica 07 Terminada
 
 #define LASER_DOWNSAMPLING  10
 #define SENSOR_NOISE        0.1
