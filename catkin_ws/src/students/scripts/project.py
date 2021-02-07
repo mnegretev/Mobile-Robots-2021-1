2a03d3da2209a7d2863faa214d8890abc7af5e4c
Mariana Arguelles Macosay <marianita_maple@comunidad.unam.mx>
2021-02-07 06:35:39 -0600

    Proyecto Terminado

--- catkin_ws/src/students/scripts/project.py
+++ catkin_ws/src/students/scripts/project.py
@@ -7,6 +7,7 @@
 import sys
 import rospy 
 
+
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PoseStamped
 from sound_play.msg import SoundRequest
@@ -14,6 +15,9 @@ from std_msgs.msg import String
 
 NAME = "ARGUELLES_MACOSAY"
 
+confirm=0
+actual_pos=1
+place=0
 #####################################
 #Definir posiciones(coordenadas) de los lugares
 #####################################
@@ -24,40 +28,72 @@ NAME = "ARGUELLES_MACOSAY"
 #KITCHEN(8,5)
 def position(order):
     dir= PoseStamped()
-    if order==1:
+    global confirm
+    if (order==1):
+        robot_talk("going to the entrance")
         posx,posy=(0,0)
-    elif order==2:
+        confirm=0
+    elif (order==2):
+        robot_talk("going to the bedroom")
         posx,posy=(6,0)
-    elif order==3:
+        confirm=0
+    elif (order==3):
+        robot_talk("going to the livingroom")
         posx,posy=(5,5)
-    elif order==4:
+        confirm=0
+    elif (order==4):
+        robot_talk("going to the corridor")
         posx,posy=(3,0)
-    elif order==5:
+        confirm=0
+    elif (order==5):
+        robot_talk("going to the kitchen")
         posx,posy=(8,5)
+        confirm=0
     dir.pose.position.x=posx
     dir.pose.position.y=posy
-    dir.pose.position.w=0
+    dir.pose.orientation.w=0
     publishing_dir.publish(dir)
-    print("goal:"+ posx+","+posy)
+    pos_ant=posx,posy
+    print("goal:"+ str(pos_ant))
     return
 
 def listening(order):
     order_received=order.data
-    if (order_received=="ROBOT GO TO THE ENTRACE"):
-        #robot_talk("did you said go to the entrace")
-        position(1)
-    elif (order_received=="ROBOT GO TO THE BEDROOM"):
-        #robot_talk("did you said go to the bedroom")
-        position(2)
-    elif (order_received=="ROBOT GO TO THE LIVINGROOM"):
-        #robot_talk("did you said go to the livingroom")
-        position(3)
-    elif (order_received=="ROBOT GO TO THE CORRIDOR"):
-        #robot_talk("did you said go to the corridor")
-        position(4)
-    elif (order_received=="ROBOT GO TO THE KITCHEN"):
-        #robot_talk("did you said go to the kitchen")
-        position(5)
+    global confirm
+    global actual_pos
+    global place
+    if(confirm == 0):
+        if (order_received=="ROBOT GO TO THE ENTRANCE"):
+            robot_talk("did you said go to the entrance")
+            place=1
+            confirm=1
+        elif (order_received=="ROBOT GO TO THE BEDROOM"):
+            robot_talk("did you said go to the bedroom")
+            place=2
+            confirm=1
+        elif (order_received=="ROBOT GO TO THE LIVINGROOM"):
+            robot_talk("did you said go to the livingroom")
+            place=3
+            confirm=1
+        elif (order_received=="ROBOT GO TO THE CORRIDOR"):
+            robot_talk("did you said go to the corridor")
+            place=4
+            confirm=1
+        elif (order_received=="ROBOT GO TO THE KITCHEN"):
+            robot_talk("did you said go to the kitchen")
+            place=5
+            confirm=1
+    elif (confirm==1):
+        if(order_received=="YES"):
+            if (place==actual_pos):
+                robot_talk("im already in the place")
+                confirm=0
+            else:
+                position(place)
+                actual_pos=place
+        elif(order_received=="NO"):
+            robot_talk("sorry try again i didnt listen well")
+            confirm=0
     else:
         robot_talk("I didn't recognice the voice command")
         
@@ -81,11 +117,13 @@ def main():
     publishing_dir=rospy.Publisher('/move_base_simple/goal', PoseStamped,queue_size=10)
     publishing_robot_voice=rospy.Publisher('/robotsound',SoundRequest,queue_size=10)
     rospy.Subscriber("/recognized",String,listening)
+    robot_talk("Were do you want me to go")
     rospy.Rate(50)
     rospy.spin()
 
 if __name__ == '__main__':
     try:
+        place=0
         main()
     except rospy.ROSInterruptException:
         pass
